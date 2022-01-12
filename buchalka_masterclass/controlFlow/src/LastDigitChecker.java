3733b579411420cca1cad39ceef60370f40cff5c
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-12 10:00:10 +0800

    Checking if any two of the three given numbers have the same last digit

---
+++ buchalka_masterclass/controlFlow/src/LastDigitChecker.java
@@ -0,0 +1,39 @@
+public class LastDigitChecker {
+
+    public static void main(String[] args) {
+        System.out.println(hasSameLastDigit(41, 22, 71)); // true
+        System.out.println(hasSameLastDigit(23, 32, 42)); // true
+        System.out.println(hasSameLastDigit(9, 99, 999)); // false
+    }
+
+
+    public static boolean hasSameLastDigit(int num1, int num2, int num3){
+        if (!isValid(num1) || !isValid(num2) || !isValid(num3)){
+            return false;
+        }
+
+        int num1LastDigit = num1 % 10;
+        int num2LastDigit = num2 % 10;
+        int num3LastDigit = num3 % 10;
+
+        if (num1LastDigit == num2LastDigit) {
+            return true;
+        }
+        else if(num1LastDigit == num3LastDigit) {
+            return true;
+        }
+        else if(num2LastDigit == num3LastDigit){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValid(int num){
+        if(num >= 10 && num <= 1000){
+            return true;
+        }
+        return false;
+    }
+
+
+}
