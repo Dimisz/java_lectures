a3b6ec5a5f8b3fd9581b8d9d3a5e78730c3a6cba
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-13 11:37:17 +0800

    Printing diagonal star

---
+++ buchalka_masterclass/controlFlow/src/DiagonalStar.java
@@ -0,0 +1,31 @@
+public class DiagonalStar {
+
+    public static void main(String[] args) {
+        printSquareStar(8);
+    }
+
+    public static void printSquareStar(int number){
+        if(number < 5){
+            System.out.println("Invalid Value");
+        }
+        else{
+            for(int row = 1; row <= number; row++){
+                for (int col = 1; col <= number; col++){
+                    if(row == 1 || col == 1 || row == number || col == number){
+                        System.out.print("*");
+                    }
+                    else if(row == col){
+                        System.out.print("*");
+                    }
+                    else if(col == (number - row + 1)){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
