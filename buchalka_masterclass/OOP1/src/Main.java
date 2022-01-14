65738db0bf5f65842b45db7f0643485a22e9af8d
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-14 16:09:44 +0800

    Added a Person class

--- buchalka_masterclass/OOP1/src/Main.java
+++ buchalka_masterclass/OOP1/src/Main.java
@@ -10,5 +10,22 @@ public class Main {
         calculator.setSecondNumber(0);
         System.out.println("multiply= " + calculator.getMultiplicationResult());
         System.out.println("divide= " + calculator.getDivisionResult());
+
+
+        //TESTING PERSON CLASS
+        System.out.println("\n\n=========TESTING PERSON CLASS========");
+        Person person = new Person();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+
     }
 }
