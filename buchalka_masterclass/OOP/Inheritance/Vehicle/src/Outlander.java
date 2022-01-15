0a3e33f16a2c0450b41b19be0c2b33bba2180ec0
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-16 07:52:04 +0800

    Car class - inheritance

--- buchalka_masterclass/OOP/Inheritance/Vehicle/src/Outlander.java
+++ buchalka_masterclass/OOP/Inheritance/Vehicle/src/Outlander.java
@@ -5,4 +5,28 @@ public class Outlander extends Car{
         super("Outlander", "4WD", 5, 5, 6, false);
         this.roadServiceMonths = roadServiceMonths;
     }
+
+    public void accelerate(int rate){
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0){
+            stop();
+            changeCurrentGear(1);
+        }
+        else if (newVelocity > 0 && newVelocity <= 10){
+            changeCurrentGear(1);
+        }
+        else if (newVelocity > 10 && newVelocity <= 20){
+            changeCurrentGear(2);
+        }
+        else if (newVelocity > 20 && newVelocity <= 30){
+            changeCurrentGear(3);
+        }
+        else{
+            changeCurrentGear(4);
+        }
+
+        if(newVelocity > 0){
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+    }
 }
