fd181a281c39ab07359d286896888ed4b83d15f0
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-15 13:45:35 +0800

    Lecture on inheritance

---
+++ buchalka_masterclass/OOP/Inheritance/src/Fish.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+
+public class Fish extends Animal{
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    private void rest(){
+        System.out.println("Resting...");
+    }
+
+    private void swim(int speed){
+        System.out.println("Moving muscles and back fin");
+        super.move(speed);
+    }
+}
