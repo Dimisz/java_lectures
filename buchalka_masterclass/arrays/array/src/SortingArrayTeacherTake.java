706a8b03e0466748f5dfd4d43d10fb0a1eda863f
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-22 10:34:18 +0800

    Sorting array method

--- buchalka_masterclass/arrays/array/src/SortingArrayTeacherTake.java
+++ buchalka_masterclass/arrays/array/src/SortingArrayTeacherTake.java
@@ -38,7 +38,7 @@ public class SortingArrayTeacherTake {
 
     public static void printArray(int[] arr){
         for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+            System.out.println("Element " + i + " contents " + arr[i]);
         }
     }
 
