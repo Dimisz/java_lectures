70ccdf1d9cacafb3e4f43877de3b2b811ed1b870
Vladimir Solovyov <leilyngale@icloud.com>
2022-01-26 11:10:02 +0800

    Lecture on Autoboxing

--- buchalka_masterclass/arrays/Autoboxing/src/Main.java
+++ buchalka_masterclass/arrays/Autoboxing/src/Main.java
@@ -26,5 +26,27 @@ public class Main {
 
         ArrayList<IntClass> intClassArrayList = new ArrayList<IntClass>();
         intClassArrayList.add(new IntClass(55));
+
+        ArrayList<Integer> intArrayList = new ArrayList<Integer>();
+        for(int i = 0; i <= 10; i++){ //AUTOBOXING
+            intArrayList.add(Integer.valueOf(i));
+        }
+
+        for(int i = 0; i <= 10; i++){ //UNBOXING
+            System.out.println(i + " -> " + intArrayList.get(i).intValue());
+        }
+
+        Integer myIntValue = 56; // Integer.valueOf(56);
+        int myInt = myIntValue; // myIntValue.intValue();
+
+        ArrayList<Double> myDoubleValues = new ArrayList<Double>();
+        for(double dbl = 0.0; dbl <= 10.0; dbl += 0.5){
+            myDoubleValues.add(Double.valueOf(dbl));
+        }
+        for(int i = 0; i < myDoubleValues.size(); i++){
+            double value = myDoubleValues.get(i).doubleValue();
+            System.out.println(i + " -> " + value);
+        }
+
     }
 }
