3af9b0829fb889184ccad428ac55b5999503bce0
Vladimir Solovyov <leilyngale@icloud.com>
2022-02-17 10:24:37 +0800

    Lecture on interfaces

--- buchalka_masterclass/ClassesInterfaces/Interfaces/src/Main.java
+++ buchalka_masterclass/ClassesInterfaces/Interfaces/src/Main.java
@@ -3,7 +3,14 @@ public class Main {
     public static void main(String[] args) {
         ITelephone office = new DeskPhone(12345);
         office.powerOn();
-        office.callPhone(4321);
+        office.callPhone(12345);
         office.answer();
+
+        ITelephone cellPhone = new MobilePhone(122222);
+//        cellPhone.powerOn();
+        cellPhone.callPhone(122222);
+        cellPhone.answer();
+        cellPhone.dial(222);
+
     }
 }
