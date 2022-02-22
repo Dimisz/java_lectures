a048a01e17b286f4dd82dc083583f9a3836db99f
Vladimir Solovyov <leilyngale@icloud.com>
2022-02-22 16:21:53 +0800

    Started the course on basics of java

---
+++ 1Z0-819OCP/zaurBasics/src/Lesson4/BankAccount.java
@@ -0,0 +1,11 @@
+package Lesson4;
+
+public class BankAccount {
+    int id;
+    String name;
+    double balance;
+
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount();
+    }
+}
