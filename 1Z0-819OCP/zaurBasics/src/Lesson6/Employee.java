6064a7441e3e41692b53fef7d39436271f9143e1
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-02 11:35:50 +0300

    Lesson 6: Methods Overloading

---
+++ 1Z0-819OCP/zaurBasics/src/Lesson6/Employee.java
@@ -0,0 +1,31 @@
+package Lesson6;
+
+public class Employee {
+    int id;
+    String name;
+    int age;
+    double salary;
+    String department;
+
+    Employee(int id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    Employee(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString(){
+        return "Name: " + name + "\nAge: " + age + "\nID: " + id;
+    }
+}
+
+class EmployeeTest{
+    public static void main(String[] args) {
+        Employee emp = new Employee(1, "Vladimir Solovyov", 34);
+        System.out.println(emp);
+    }
+}
