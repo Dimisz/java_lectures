1efa53f07cefc4b2dd43f2cbce1acbbf0d7feefe
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-05 16:54:09 +0300

    Lessons 12 & 13

---
+++ 1Z0-819OCP/zaurBasics/src/Lesson12/Test10.java
@@ -0,0 +1,10 @@
+package Lesson12;
+
+public class Test10 {
+    public static void main(String[] args) {
+        int a = 10;
+        if(a < 20){
+            System.out.println("a less than 20");
+        }
+    }
+}
