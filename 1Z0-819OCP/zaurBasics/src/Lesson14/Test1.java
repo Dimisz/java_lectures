4297a6be1295d62b0ab1dcfe2bd0ef7a4988f62b
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-12 17:36:55 +0300

    Started Lesson 14

---
+++ 1Z0-819OCP/zaurBasics/src/Lesson14/Test1.java
@@ -0,0 +1,10 @@
+package Lesson14;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            System.out.println("Current counter: " + i);
+        }
+    }
+}
