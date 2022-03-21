1436a2d2ca400ee9e607011880f840d60b9d03cf
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-21 11:49:02 +0300

    ArrayList

--- 1Z0-819OCP/zaurAdvanced/Collections/src/collection/ArrayListEx1.java
+++ 1Z0-819OCP/zaurAdvanced/Collections/src/collection/ArrayListEx1.java
@@ -1,6 +1,7 @@
 package collection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListEx1 {
     public static void main(String[] args) {
@@ -13,6 +14,22 @@ public class ArrayListEx1 {
         ArrayList<String> arrayList2 = new ArrayList<>();
         arrayList2.add("Michael");
         arrayList2.add("Peter");
+
+
+        //best practice is to mention initial capacity
+        ArrayList<String> arrayList3 = new ArrayList<>(200);
+        arrayList3.add("AAA");
+        arrayList3.add("BBB");
+        System.out.println(arrayList3.size());//capacity is NOT size
+        List<String> arrayList4 = new ArrayList<>();
+
+        ArrayList<String> arrayList5 = new ArrayList<>(arrayList1);
+        System.out.println(arrayList1 == arrayList5);
+        System.out.println(arrayList5);
+        arrayList1.add("Pete");
+        System.out.println(arrayList1);
+        System.out.println(arrayList5);
+
 
     }
 }
