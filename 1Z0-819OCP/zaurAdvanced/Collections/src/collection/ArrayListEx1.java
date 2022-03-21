602d66e5a3279c013e8f8b29519d708204069430
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-21 11:41:09 +0300

    ArrayList

---
+++ 1Z0-819OCP/zaurAdvanced/Collections/src/collection/ArrayListEx1.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<String>();
+        arrayList1.add("Cady");
+        arrayList1.add("Vladimir");
+        arrayList1.add("John");
+        System.out.println(arrayList1);
+
+        ArrayList<String> arrayList2 = new ArrayList<>();
+        arrayList2.add("Michael");
+        arrayList2.add("Peter");
+
+    }
+}
