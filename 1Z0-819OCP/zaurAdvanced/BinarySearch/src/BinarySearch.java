5b0d25582d1bfb4d2bf3f21d5e9ceb93edbe967a
Vladimir Solovyov <leilyngale@icloud.com>
2022-03-27 15:40:51 +0300

    Binary Search

--- 1Z0-819OCP/zaurAdvanced/BinarySearch/src/BinarySearch.java
+++ 1Z0-819OCP/zaurAdvanced/BinarySearch/src/BinarySearch.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        /*
+
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(-3);
         arrayList.add(8);
@@ -18,15 +19,24 @@ public class BinarySearch {
         arrayList.add(-30);
         arrayList.add(19);
 
-        int index = Collections.binarySearch(arrayList, 12);
-        System.out.println(index);
+//        int index = Collections.binarySearch(arrayList, 12);
+//        System.out.println(index);
+
 
         Collections.sort(arrayList);
+        System.out.println(arrayList);
+        Collections.reverse(arrayList);
+        System.out.println(arrayList);
+        Collections.shuffle(arrayList);
+        System.out.println(arrayList);
+        /*
         index = Collections.binarySearch(arrayList, 12);
         System.out.println(index);
         index = Collections.binarySearch(arrayList, 121);
         System.out.println(index);
         */
+
+        /*
         Employee emp1 = new Employee(100, "Zaur", 12345);
         Employee emp2 = new Employee(15, "Ivan", 6542);
         Employee emp3 = new Employee(123, "Peter", 8542);
@@ -47,7 +57,17 @@ public class BinarySearch {
         System.out.println(employees);
         int index2 = Collections.binarySearch(employees, new Employee(123, "Peter", 8542));
         System.out.println(index2);
+        */
 
+        /*
+        int[] nums = {-3, 8, 12, -8, 0, 5, 10, 1, 150, -30, 19};
+        System.out.println(Arrays.toString(nums));
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        int index3 = Arrays.binarySearch(nums, 150);
+        System.out.println(index3);
+
+         */
     }
 }
 
