e62576806412aea57443a78b6c78ff2cea3f3188
Vladimir Solovyov <leilyngale@icloud.com>
2022-05-02 17:09:45 +0300

    Generics intro

---
+++ 1Z0-819OCP/zaurAdvanced/GenericsModule/src/generics/GenericsIntro.java
@@ -0,0 +1,19 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsIntro {
+    public static void main(String[] args) {
+        /*
+        // RAW TYPE
+        // possible but not recommended
+
+        List list = new ArrayList<>();
+        list.add("Ok");
+        list.add(5);
+        list.add(new StringBuilder("abcde"));
+        */
+
+    }
+}
